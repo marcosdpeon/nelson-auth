9875ca7718b73b868a680f0e8804c0feea425ea8
light7dream <ry.mc.le.92@gmail.com>
2023-05-22 16:32:30 -0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/mydatabase"
-  };
+  // url: "mongodb://localhost:27017/mydatabase"
+  url: "mongodb+srv://marcosdpeon:jeupIs1QORObX1z7@cluster0.vorzpr1.mongodb.net/nelson-auth"
+};
